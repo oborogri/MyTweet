b3ba2853c5cded6ff57fa5519957219db7c69e2d
oborogri <grigore.oboroceanu@gmail.com>
2016-10-17 12:55:19 +0100

    MyTweet_v_01(TimelineList refactored to fragments)

--- app/src/main/java/app/mytweet/activities/TimelineListActivity.java
+++ app/src/main/java/app/mytweet/activities/TimelineListActivity.java
@@ -1,98 +1,24 @@
 package app.mytweet.activities;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-
 import app.mytweet.R;
-import app.mytweet.app.MyTweetApp;
-import app.mytweet.models.Portfolio;
-import app.mytweet.models.Tweet;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
-public class TimelineListActivity extends AppCompatActivity {
-
-    private ListView listView;
-    private Portfolio portfolio;
-    private TweetAdapter adapter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timelinelist);
-
-        listView = (ListView) findViewById(R.id.timelineList);
-
-        MyTweetApp app = (MyTweetApp) getApplication();
-        portfolio = app.portfolio;
-
-        adapter = new TweetAdapter(this, portfolio.tweets);
-        listView.setAdapter(adapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu_timeline, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.menu_item_new_tweet:
-                startActivity (new Intent(this, MyTweetActivity.class));
-                return true;
-
-            case R.id.menu_logout:
-                startActivity (new Intent(this, WelcomeActivity.class));
-                return true;
-
-            default: return super.onOptionsItemSelected(item);
-        }
-    }
-}
-
-class TweetAdapter extends ArrayAdapter<Tweet>
+public class TimelineListActivity extends AppCompatActivity
 {
-    private Context context;
-
-    public TweetAdapter(Context context, ArrayList<Tweet> tweets)
+    public void onCreate(Bundle savedInstanceState)
     {
-        super(context, 0, tweets);
-        this.context = context;
-    }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_container);
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if (convertView == null)
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null)
         {
-            convertView = inflater.inflate(R.layout.timelinelist_item, null);
+            fragment = new TimelineListFragment();
+            manager.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
         }
-        Tweet t = getItem(position);
-
-        TextView text = (TextView) convertView.findViewById(R.id.timelinelist_item_text);
-        text.setText(t.text);
-
-        TextView dateTextView = (TextView) convertView.findViewById(R.id.timelinelist_item_dateTextView);
-        dateTextView.setText(t.getDateString());
-
-        return convertView;
     }
 }
---
+++ app/src/main/java/app/mytweet/activities/TimelineListFragment.java
@@ -0,0 +1,134 @@
+package app.mytweet.activities;
+
+import java.util.ArrayList;
+
+import app.mytweet.app.MyTweetApp;
+import app.mytweet.helpers.IntentHelper;
+import app.mytweet.R;
+import app.mytweet.app.MyTweetApp;
+import app.mytweet.models.Portfolio;
+import app.mytweet.models.Tweet;
+
+import android.widget.ListView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.CheckBox;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class TimelineListFragment extends ListFragment implements OnItemClickListener
+{
+    private ArrayList<Tweet> tweets;
+    private Portfolio portfolio;
+    private MyTweetAdapter adapter;
+    MyTweetApp app;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        getActivity().setTitle(R.string.app_name);
+
+        app = MyTweetApp.getApp();
+        portfolio = app.portfolio;
+        tweets = portfolio.tweets;
+
+        adapter = new MyTweetAdapter(getActivity(), tweets);
+        setListAdapter(adapter);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = super.onCreateView(inflater, parent, savedInstanceState);
+        return v;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Tweet t = ((MyTweetAdapter) getListAdapter()).getItem(position);
+        Intent i = new Intent(getActivity(), MyTweetActivity.class);
+        i.putExtra(TweetFragment.EXTRA_TWEET_ID, t.id);
+        startActivityForResult(i, 0);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((MyTweetAdapter) getListAdapter()).notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_timeline, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_new_tweet:
+                Tweet tweet = new Tweet();
+                portfolio.addTweet(tweet);
+
+                Intent i = new Intent(getActivity(), MyTweetActivity.class);
+                i.putExtra(TweetFragment.EXTRA_TWEET_ID, tweet.id);
+                startActivityForResult(i, 0);
+                return true;
+
+            case R.id.menu_logout:
+                Intent in = new Intent(getActivity(), WelcomeActivity.class);
+                startActivityForResult(in, 0);
+
+                //startActivity (new Intent(this, WelcomeActivity.class));
+                //return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Tweet t = adapter.getItem(position);
+        IntentHelper.startActivityWithData(getActivity(), MyTweetActivity.class, "TWEET_ID", t.id);
+    }
+
+    class MyTweetAdapter extends ArrayAdapter<Tweet>
+    {
+        private Context context;
+
+        public MyTweetAdapter(Context context, ArrayList<Tweet> tweets) {
+            super(context, 0, tweets);
+            this.context = context;
+        }
+
+        @SuppressLint("InflateParams")
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.timelinelist_item, null);
+            }
+            Tweet t = getItem(position);
+
+            TextView textTweet = (TextView) convertView.findViewById(R.id.timelinelist_item_text);
+            textTweet.setText(t.text);
+
+            TextView dateTextView = (TextView) convertView.findViewById(R.id.timelinelist_item_dateTextView);
+            dateTextView.setText(t.getDateString());
+
+            return convertView;
+        }
+    }
+}
